1403bbcc5d49b9aae79e4510605ffc486fcccba4
edwardbayron <edwardbayron@gmail.com>
2026-05-08 22:48:08 +0300

    upgrade target and compileSdk to run

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.luminor.luminortestproject"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.luminor.luminortestproject"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
